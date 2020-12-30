eda9718fc3f3bbb1c2d73559f4eb55ec49a1006d
Calvin <calvindass8761@gmail.com>
2020-12-30 19:59:38 +1100

    Added pseudo code for character generation

--- commands/build_character.js
+++ commands/build_character.js
@@ -5,6 +5,11 @@ const message_options = {
     errors: ['time']
 }
 
+// Used for character selection
+const dice_role = (sides = 10) => {
+    return Math.ceil(Math.random()*sides)
+}
+
 class Character{
     constructor(dm){
         this.dm = dm
@@ -12,6 +17,19 @@ class Character{
         this.age = undefined
         this.role = undefined
         this.pronouns = undefined
+        this.characteristics = {
+            "int": 0,
+            "emp": 0,
+            "ref": 0,
+            "cl": 0,
+            "tech": 0,
+            "att": 0,
+            "ma": 0,
+            "lk": 0 
+        }
+        this.style = undefined
+        this.ethnicity = undefined
+        this.eurodollars = 500
     }
 
     setCharacterName = () => {
@@ -29,7 +47,7 @@ class Character{
     setCharacterAge = () => {
         return new Promise((resolve, reject) => {
             let filter = m => m.author.id == this.dm.recipient.id
-            this.dm.send(`How old is ${this.name}?`)
+            this.dm.send(`How old is ${this.name}? (Must be more than 16!)`)
             this.dm.awaitMessages(filter, message_options).then(messages => {
                 let message = messages.first()
                 this.age = message.content
@@ -59,6 +77,29 @@ class Character{
         -----------END------------`
     }
 
+    generateLifeStory = () => {
+        // Family Background
+            // Family Ranking - Picked
+                // 4, 6, 10 get siblings
+            // Parents - Random
+            // Something Happened to your Parents - Random
+            // Family Status - Random
+            // Family Tragedy - Random
+            // Childhood Environment - Picked
+            // Picked
+        
+        // Motivations
+            // All picked
+
+        // Big Problems, Big Wins
+            // Automate every year past 16\
+        
+        // Friends and Enemies
+            // Automate
+        
+        // Romantic Life
+            // Automate
+    }
 }
 
 
@@ -69,8 +110,7 @@ module.exports = {
 	execute(message, args) {
         message.guild.members.fetch().then((users) => {
             let GM = users.find(user => user.roles.cache.find(r => r.name == "GM"))
-            let GM_direct = GM.createDM();
-
+            let GM_direct = GM.createDM(); 
             users.forEach((user) => {
                 if(!user.user.bot && user.roles.cache.find(r => r.name == "Player")){
                     let direct_message = user.createDM();
